2f8350176f4984b7240869bace2c7a03168da5d3
Jorge Souza <jorge@vieirasouza.com.br>
2021-04-06 21:03:51 -0300

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+/*Now that our server is listening for requests being made on localhost:8000 
+let's return some mock JSON data. Add the following to your server.js file:*/
+
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+app.get("/users", function (req, res) {
+  res.json({
+    succes: true,
+    message: "succesfully got users. Nice!",
+    users: mockUserData,
+  });
+});
+
 app.listen(8000, function () {
-  console.log("server is running");
+  console.log("server is listening");
 });
